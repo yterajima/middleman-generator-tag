c8ec88d71bce848bfd7c85ed9cde0ce7654d6a11
yterajima <terra@e2esound.com>
2015-04-01 13:41:23 +0900

    Add feature: Insert generator tag

--- lib/middleman-generator-tag/extension.rb
+++ lib/middleman-generator-tag/extension.rb
@@ -3,6 +3,17 @@ module Middleman
     class Extension < ::Middleman::Extension
       def initialize(app, options_hash = {}, &block)
         super
+
+        tag = generator_tag
+        app.after_render do |content, path, locs|
+          if /<html>/ =~ content.to_s
+            content.sub!(/<\/head>/, "#{tag}\n</head>")
+          end
+        end
+      end
+
+      def generator_tag
+        "<meta name=\"generator\" content=\"Middleman v#{::Middleman::VERSION}\">"
       end
     end
   end
--- middleman-generator-tag.gemspec
+++ middleman-generator-tag.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
 
   spec.required_ruby_version = '>= 2.0.0'
 
-  spec.add_runtime_dependency "middleman", ">= 4.0.0.alpha"
+  spec.add_runtime_dependency "middleman", "~> 3.3"
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
